c112b6c9fcdb68ca2fbedaf4572059fa03da1be8
Jachithracs <jachithra.cs@ust.com>
2023-10-18 17:25:20 +0530

    Add project files.

---
+++ Basic Programs/Electricity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+     class Electricity
+    {
+        public int consumernumber, prevreading, currentReading;
+        public string? consumername;
+
+        //public Electricity() 
+        //{ 
+        //    consumernumber = 12345;//0
+        //    prevreading = 9000;
+        //    currentReading = 9300;
+        //    consumername = "XXX"; //String.Empty;
+
+        //}
+
+        public Electricity(int consumernumber, int prevreading, int currentReading, string? consumername)
+        {
+            this.consumernumber = consumernumber;
+            this.prevreading = prevreading;
+            this.currentReading = currentReading;
+            this.consumername = consumername;
+        }
+
+        public double CalculateBill()
+        {
+            double billamount ;
+            int reading = currentReading - prevreading;
+            if (reading <= 100) 
+            {
+                billamount = reading * 2.00;
+
+            }
+            else if(reading <= 200 && reading >= 101)
+            {
+                billamount = (100 * 2) + (( reading - 100 ) * 2.5);
+            }
+            else if (reading <= 401 && reading >= 201)
+            {
+                billamount = (100 * 2) + (100 * 2.5) + ((reading - 200) * 3.5);
+               
+            }
+            else 
+            {
+                billamount = (100 * 2) + (100 * 2.5) + (200 * 3.5) + ((reading - 400) * 5.00);
+            }
+
+            return billamount;
+        }
+     }
+}
---
+++ Basic Programs/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+Console.WriteLine("Enter two numbers :");
+int num1, num2, ans;
+num1 = Convert.ToInt32((Console.ReadLine()));
+num2 = Convert.ToInt32((Console.ReadLine()));
+
+ans = num1 + num2;
+Console.WriteLine("Result is "+ ans);
+*/
+
+/*
+string? s1 = " ";
+string? s2 = " ";
+string? s3 = " ";
+
+s1 = Console.ReadLine();
+s2 = Console.ReadLine();
+
+s3 = s1 + " " + s2;
+Console.WriteLine(s3);
+
+Console.WriteLine(s1.Equals(s2));
+Console.WriteLine(s1.ToLower());
+Console.WriteLine(s1.ToUpper());
+
+Console.WriteLine(s2.Substring(3));
+
+string[] s = s3.Split(' ');
+for(int i = 0; i < s.Length; i++)
+    Console.WriteLine(s[i]);
+*/
+
+/*
+int x = 5; //char x = '5'; string x = "5";
+switch(x)
+{
+    case 1: //case '1':  case "1":
+        Console.WriteLine();
+        break;
+    case 2:
+        Console.WriteLine();
+        break;
+    case 3:
+        Console.WriteLine();
+        break;
+    default: Console.WriteLine();
+        break;
+}
+*/
+
+
+//driver
+using Basic_Programs;
+
+/*
+Calculation calculation = new Calculation();
+
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine());
+
+ double res = calculation.Add(num1,num2);
+Console.WriteLine(res);
+*/
+
+
+//Electricity electricity = new ();
+//Electricity electricity = new Electricity();
+//double billamount = electricity.CalculateBill();
+
+int consumernumber=Convert.ToInt32(Console.ReadLine());
+
+Electricity electricity1 = new Electricity(consumernumber, 9000, 9300, "XX");
+//Electricity electricity1 = new Electricity(12345,9000,9300,"XX");
+Console.WriteLine(electricity1.consumername);
+Console.WriteLine(electricity1.consumernumber);
+Console.WriteLine(electricity1.CalculateBill());
+
+
+Electricity electricity2 = new Electricity(23456, 9000, 10000, "AA");
+Console.WriteLine(electricity2.consumername);
+Console.WriteLine(electricity2.consumernumber);
+Console.WriteLine(electricity2.CalculateBill());
+
+
+
+
