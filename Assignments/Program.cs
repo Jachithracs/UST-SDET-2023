5dfcef089019cca72a79816eb542b6b4134d868c
Jachithracs <jachithra.cs@ust.com>
2023-10-19 04:14:39 +0530

    Assignment uploaded

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,2 +1,20 @@
 ﻿// 18-10-2023
 
+using Assignments;
+
+
+Console.WriteLine("Enter name :");
+string? StudentName = Console.ReadLine();
+Console.WriteLine("Enter first mark :");
+double markOne = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter second mark :");
+double markTwo = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter third mark :");
+double markThree = double.Parse(Console.ReadLine());
+
+Student student = new Student(StudentName, markOne, markTwo, markThree);
+
+Console.WriteLine("Student Name is : " + StudentName);
+Console.WriteLine("Total Mark is : " + student.TotalMark());
+Console.WriteLine("Grade is : " +student.DisplayGrade());
+
--- Assignments/Student.cs
+++ Assignments/Student.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace Assignments
 {
-    internal class Student
+    class Student
     {
+        public string? StudentName;
+        double markOne, markTwo, markThree;
+
+        public Student(string? studentName, double markOne, double markTwo, double markThree)
+        {
+            StudentName = studentName;
+            this.markOne = markOne;
+            this.markTwo = markTwo;
+            this.markThree = markThree;
+        }
+
+
+        public double CalculateAverage()
+        {
+            double totalMark, averageMark;
+           
+
+            totalMark = markOne + markTwo + markThree;
+
+            averageMark = totalMark / 3;
+
+            return averageMark;
+        }
+
+        public string DisplayGrade()
+        {
+            string? grade;
+            double average = CalculateAverage();
+
+            if (average > 80)
+            {
+                grade = "A";
+            }
+            else if (average > 60)
+                 {
+                    grade = "B";
+                 }
+            else if (average > 40)
+            {
+                grade = "C";
+            }
+            else 
+            {
+                grade = "Fail";
+            }
+            return grade;
+        }
+
+        public double TotalMark()
+        {
+            return markOne + markTwo + markThree;
+        }
     }
 }
